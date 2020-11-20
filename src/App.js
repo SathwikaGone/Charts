d819f8da7ec45c9d5ecbd61d0e0adc7a53ba7013
SathwikaGone <gonesathwika@gmail.com>
2020-11-20 13:44:17 -0500

    PieChart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import BarChart from "./Components/BarChart";
 import LineChart from "./Components/LineChart";
+import PieChart from "./Components/PieChart";
 function App() {
   return (
     <div className="App">
       <h2>Chart.js in React.js</h2>
       <div className="Appp">
         <BarChart />
+        <LineChart />
+      </div>
+      <div className="Appp">
+        <PieChart />
         <LineChart />
       </div>
     </div>
---
+++ src/Components/PieChart.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { data } from "./Data";
+export default function BarChart() {
+  const options = {
+    title: {
+      display: true,
+      text: "Pie Chart",
+    },
+    maintainAspectRatio: false,
+    legend: {
+      display: true,
+      position: "top",
+    },
+  };
+  return (
+    <div style={{ width: "550px", height: "500px" }}>
+      <Pie data={data.bar} options={options} />
+    </div>
+  );
+}
